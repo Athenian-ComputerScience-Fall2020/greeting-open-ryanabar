0fa83bd6a006d7a7c86a270da46277288272d2c2
ryanabar <21rabar@athenian.org>
2020-09-17 16:10:07 +0000

    UPDATED FINAL

--- my_code.py
+++ my_code.py
@@ -1,7 +1,7 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  None
 
 # Write a program that asks for the user's name and another piece of information.Then prints a response using both of the inputs.
-firstName = input("Whats your name?")
-lastName = input("What's your last name?")
-    print ("Hello " + firstName + lastName)
+firstName = input("Whats your name? ")
+lastName = input("What's your last name? ")
+print ("Hello " + firstName + " " + lastName)
